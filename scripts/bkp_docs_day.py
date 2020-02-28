460d819e34e4244c04d81c6a977fcaf675c10c92
Daniel Oshkin <it.danya.git@gmail.com>
2020-02-28 21:29:12 +0500

    modify a script

--- scripts/bkp_docs_day.py
+++ scripts/bkp_docs_day.py
@@ -1,6 +1,7 @@
 import os
 import time
 import zipfile
+import sys
 
 # задание переменных
 SRC_DIR = '/home/danya/python' 
@@ -13,16 +14,21 @@ now = time.strftime('%d-%m-%Y')
 
 # прверяем есть ли каталог, который хотим бэкапить
 if not os.path.exists(SRC_DIR):
-    print('Не существует каталога для архивирования - ' + SRC_DIR)
+    f = open('error.txt', 'w+')
+    f.write('Не существует каталога для архивирования - ' + SRC_DIR)
+    f.write('\nБэкап не сделан ...')
+    f.close()
 else:
-    print('Каталог ' + SRC_DIR + ' существет, можем начинать')
-
-zipp=zipfile.ZipFile(DST_DIR + now + '_new.zip','w')
-for root, dirs, files in os.walk(SRC_DIR):
-    for file in files:
-        zipp.write(os.path.join(root, file))
-zipp.close()
-
-
+    f = open('backup.txt', 'w+')
+    f.write('Каталог ' + SRC_DIR + ' существет, начало создания бэкапа' + ' - ' 
+            + str(time.asctime()))
+#    sys.stdout = open('backup.txt', 'w')
+    zipp=zipfile.ZipFile(DST_DIR + BKP_NAME + now + '.zip','w')
+    for root, dirs, files in os.walk(SRC_DIR):
+        for file in files:
+           zipp.write(os.path.join(root, file))
+    zipp.close()
+    f.write('\nБэкап создан в' + ' - ' + str(time.asctime()))
+    f.close()
 
 
